6c23291b9e930067e7e7a8f03085acdc38dadb53
Sáng Nguyễn Quang <sangnq.dev@gmail.com>
2023-09-17 19:27:59 +0700

    add transition

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,25 @@
 <template>
-  <router-view />
+  <div class="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/demo">Demo</router-link>
+    <router-link to="/counter">Counter</router-link>
+  </div>
+
+  <router-view v-slot="{ Component }">
+    <Transition
+      name="fade"
+    >
+      <component :is="Component" />
+    </Transition>
+  </router-view>
 </template>
+
+<style>
+  .nav {
+    margin-bottom: 20px;
+  }
+
+  .nav a {
+    padding-right: 10px;
+  }
+</style>
--- src/modules/home/ViewHome.vue
+++ src/modules/home/ViewHome.vue
@@ -1,3 +1,18 @@
+<script setup>
+  import { ref } from "vue";
+
+  const show = ref(false);
+  const handleToggle = () => {
+    show.value = !show.value;
+  };
+</script>
+
 <template>
-  <div class="home">Home page</div>
+  <div class="home">
+    <Transition name="fade">
+      <span v-if="show">Home page</span>
+    </Transition>
+    <br />
+    <button @click="handleToggle">Toggle</button>
+  </div>
 </template>
