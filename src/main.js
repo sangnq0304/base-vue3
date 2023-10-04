3fffa1ec56fc2a0d132798c020cd62326f171f9d
Sáng Nguyễn Quang <sangnq.dev@gmail.com>
2023-10-04 21:32:05 +0700

    add element-plus, scss, tailwindCss

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import './assets/main.css'
+import './index.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,8 +7,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
+app.use(ElementPlus)
 app.mount('#app')
